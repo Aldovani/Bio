a0f74b269c4255357253fcc9ccfac1b1a5e4d49d
Aldovani <aldovanihcosta@gmail.com>
2022-07-08 21:42:15 -0300

    add seo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,23 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta name="title" content="Aldovani Henrique da costa" />
+        <meta name="description" content="Frontend Developer" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://aldovani-bio.vercel.app/" />
+        <meta property="og:title" content="Aldovani Henrique da costa" />
+        <meta property="og:description" content="Frontend Developer" />
+        <meta property="og:image" content="./capa.svg" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://aldovani-bio.vercel.app/"
+        />
+        <meta property="twitter:title" content="Aldovani Henrique da costa" />
+        <meta property="twitter:description" content="Frontend Developer" />
+        <meta property="twitter:image" content="./capa.svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
